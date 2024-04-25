8ecd064877b05626d84b3d77ed4d5728d4ca4e4f
Lucas Maranho <lksribeiro2014@gmail.com>
2024-04-24 22:23:27 -0300

    estrutura inicial do layout divisão de componentes

---
+++ src/app/components/info-content/info-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './info-content.component.html',
+  styleUrl: './info-content.component.scss'
+})
+export class InfoContentComponent {
+
+}
