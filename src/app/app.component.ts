d3f2313e73ee5dd781eba17b912d47f5f67582c4
Lucas Maranho <lksribeiro2014@gmail.com>
2024-04-24 22:24:44 -0300

    estrutura inicial do layout divisão de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { BuscaComponent } from './components/busca/busca.component';
+import { CardComponent } from './components/card/card.component';
+import { InfoContentComponent } from './components/info-content/info-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, BuscaComponent, CardComponent, InfoContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
